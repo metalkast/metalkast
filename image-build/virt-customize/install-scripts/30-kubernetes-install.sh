a6900a66c3ce5c5094760701dda84d106a64df43
Luka Skugor <luka.skugor@chimeramail.com>
2023-12-16 12:14:25 +0100

    Update k8s to 1.28.4 (#28)

--- image-build/virt-customize/install-scripts/30-kubernetes-install.sh
+++ image-build/virt-customize/install-scripts/30-kubernetes-install.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 set -eEuo pipefail
 
+KUBERNETES_VERSION_MINOR=${CONFIG__KUBERNETES_VERSION%.*}
+
 # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/#installing-kubeadm-kubelet-and-kubectl
 
 # step 1
-apt-get install -y apt-transport-https ca-certificates curl
+apt-get install -y apt-transport-https ca-certificates curl gpg
 
 # step 2
-# curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-# hotfix: https://github.com/kubernetes/release/issues/2862#issuecomment-1554211504
-curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://dl.k8s.io/apt/doc/apt-key.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v${KUBERNETES_VERSION_MINOR}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 # step 3
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v${KUBERNETES_VERSION_MINOR}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 # step 4
 # ${CONFIG__KUBERNETES_VERSION} should be set by install.sh script
 apt-get update
 apt-get -yq install \
-    kubelet="${CONFIG__KUBERNETES_VERSION}-00" \
-    kubeadm="${CONFIG__KUBERNETES_VERSION}-00" \
-    kubectl="${CONFIG__KUBERNETES_VERSION}-00"
+    kubelet="${CONFIG__KUBERNETES_VERSION}-1.1" \
+    kubeadm="${CONFIG__KUBERNETES_VERSION}-1.1" \
+    kubectl="${CONFIG__KUBERNETES_VERSION}-1.1"
 apt-mark hold kubelet kubeadm kubectl
