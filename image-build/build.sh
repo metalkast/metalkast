e9be420715b7c25f678d9fd7a06bbe770a0c1859
Luka Skugor <luka.skugor@chimeramail.com>
2024-01-10 22:43:30 +0000

    Use privileged container for building k8s images to speed up builds

--- image-build/build.sh
+++ image-build/build.sh
@@ -4,8 +4,5 @@ set -eEuo pipefail
 builder_image=k8s-cluster-node-image-builder
 docker build . -t $builder_image
 
-temporary_builder_container=$(docker create $builder_image)
-docker cp $temporary_builder_container:/virt-customize/output .
-
-# Cleanup
-docker rm $temporary_builder_container
+mkdir -p output
+docker run --rm --privileged -v $(pwd)/output:/virt-customize/output $builder_image
--- image-build/publish.sh
+++ image-build/publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eEuo pipefail
 
-rclone copy -P output/ metalkast:node-images/
+rclone copy -P output/ metalkast:node-images/ --exclude "**/md5sum.txt"
