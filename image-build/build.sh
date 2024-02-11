e05323bf190e3cc420c4daf893b806d24c4e76ac
Luka Skugor <luka.skugor@chimeramail.com>
2024-02-11 16:42:02 +0000

    Revamp image build to support regular release process

--- image-build/build.sh
+++ image-build/build.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 set -eEuo pipefail
 
-builder_image=k8s-cluster-node-image-builder
-docker build . -t $builder_image
-
 mkdir -p output
-docker run --rm --privileged -v $(pwd)/output:/virt-customize/output $builder_image
+rm -rf output/*
+
+export METALKAST_VERSION=$(git describe --always --dirty)
+export NETBOOT_BASE_URL=https://dl.metalkast.io/node-images/
+
+for v in 1.28.4 1.29.1; do
+    export KUBERNETES_VERSION=$v
+    function compose() {
+        docker compose -p image-build-$(echo $v | tr '.' '_') "$@"
+    }
+    compose down
+    compose build
+    compose up --exit-code-from image-build
+    compose cp image-build:/virt-customize/output .
+done
