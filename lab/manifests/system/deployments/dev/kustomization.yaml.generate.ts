4357a73f9bf65efe3cd069fe4836e44014f9f378
Luka Skugor <luka.skugor@chimeramail.com>
2024-03-24 11:24:44 +0000

    Streamline config

--- lab/manifests/system/deployments/dev/kustomization.yaml.generate.ts
+++ lab/manifests/system/deployments/dev/kustomization.yaml.generate.ts
@@ -3,6 +3,9 @@ import { systemManifest } from "../../../../../docs/utils/manifests";
 async function main() {
     console.log(systemManifest({
         manifestsRef: "main",
+        controlPlaneHostname: "192.168.123.104",
+        ingressIP: "192.168.123.105",
+        ingressDomain: "192.168.123.105.nip.io"
     }))
 }
 
