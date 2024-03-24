599443cbe8cbccc96a3fce941bd6ee65f0c395da
Luka Skugor <luka.skugor@chimeramail.com>
2024-03-24 13:26:55 +0000

    some improvements

--- scripts/build-kustomize.sh
+++ scripts/build-kustomize.sh
@@ -1,19 +1,29 @@
-#!/bin/bash
-set -euo pipefail
+#!/usr/bin/env bash
+set -eEuo pipefail
 
-root_directory="$(git rev-parse --show-toplevel)"
-kustomize_version=$(grep 'sigs.k8s.io/kustomize/kustomize/v5' $root_directory/go.mod | awk '{print $2}')
-GOBIN=${root_directory}/bin GO111MODULE=on go install sigs.k8s.io/kustomize/kustomize/v5@$kustomize_version
+git_root_directory="$(git rev-parse --show-toplevel)"
+kustomize_version=$(grep 'sigs.k8s.io/kustomize/kustomize/v5' $git_root_directory/go.mod | awk '{print $2}')
+GOBIN=${git_root_directory}/bin GO111MODULE=on go install sigs.k8s.io/kustomize/kustomize/v5@$kustomize_version
 
-npm install && npm run generate-manifests
+npm install > /dev/null 2>&1 && npm run generate-manifests
 
-grep -rl --include="*.yaml" "kind: Kustomization" "$root_directory" |
-xargs -L1 -I_k -P$(nproc) bash -c '
-    dir=$(dirname _k)
-    echo "Running kustomize build for layer: $dir"
+exit_code=0
+for k in $(grep -rl --include="*.yaml" "kind: Kustomization" "$git_root_directory"); do
+    dir=$(dirname $k)
+    relative_path=$(realpath $dir -s --relative-to=$git_root_directory)
+    echo "Running kustomize build for layer: $relative_path"
 
-    if ! err=$(kustomize build --enable-helm $dir 2>&1 > /dev/null); then
-        echo "Build failed for kustomize layer: $dir"
-        echo "$err"
+    if ! err_output=$(kustomize build --enable-helm $dir 2>&1 > /dev/null); then
+        exit_code=1
+        echo "$(tput setaf 1)Build failed for kustomize layer: $relative_path$(tput sgr 0)"
+        echo $err_output |
+            sed -E "s#'([^ ]+)'#\1#g" |
+            sed "s#$git_root_directory/##g" |
+            sed -E "s#'[^']+'|:[^:']+#\n- \0#g" |
+            sed -E 's#^- : #- #' |
+            tr -d "'" |
+            sed 's#^#  #g'
     fi
-'
+done
+
+exit $exit_code
