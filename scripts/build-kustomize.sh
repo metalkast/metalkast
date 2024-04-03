8c6bc1210b57f9d6dccfd81854cc928ff01abe70
Luka Skugor <luka.skugor@chimeramail.com>
2024-04-03 23:17:23 +0200

    Apply suggestions from code review
    Co-authored-by: Mykola Dzham <i@levsha.me>

--- scripts/build-kustomize.sh
+++ scripts/build-kustomize.sh
@@ -2,7 +2,7 @@
 set -eEuo pipefail
 
 git_root_directory="$(git rev-parse --show-toplevel)"
-kustomize_version=$(grep 'sigs.k8s.io/kustomize/kustomize/v5' $git_root_directory/go.mod | awk '{print $2}')
+kustomize_version=$(awk '/sigs\.k8s\.io\/kustomize\/kustomize\/v5/ {print $2}' $git_root_directory/go.mod)
 GOBIN=${git_root_directory}/bin GO111MODULE=on go install sigs.k8s.io/kustomize/kustomize/v5@$kustomize_version
 
 npm install > /dev/null 2>&1 && npm run generate-manifests
@@ -14,7 +14,7 @@ for k in $(grep -rl --include="*.yaml" "kind: Kustomization" "$git_root_director
     echo "Running kustomize build for layer: $relative_path"
 
     if ! err_output=$(kustomize build --enable-helm $dir 2>&1 > /dev/null); then
-        exit_code=1
+        exit_code=$?
         echo "$(tput setaf 1)Build failed for kustomize layer: $relative_path$(tput sgr 0)"
         echo $err_output |
             sed -E "s#'([^ ]+)'#\1#g" |
